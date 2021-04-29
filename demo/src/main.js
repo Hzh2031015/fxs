f981350b6df6d722c3d37b48a349ea910d80086e
翟先生 <2744032471@qq.com>
2021-04-29 14:13:50 +0800

    最初模板

---
+++ demo/src/main.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+import http from "./http/http.js";
+Vue.prototype.$http = http;
+//全局引入http方法
+
+import Vant from "vant";
+import "vant/lib/index.css";
+Vue.use(Vant);
+//全部引入vant
+
+import { Lazyload } from "vant";
+Vue.use(Lazyload);
+//在main.js引入懒加载的指令
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
